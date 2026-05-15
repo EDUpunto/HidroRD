7fa81fae1e1628653c698de058c08265fbceb633
Juan Ernesto De Frías <edefrias@gmail.com>
2026-05-15 17:24:03 -0400

    Fix typo in cargarGeoJSONCorregido function call

--- accionLFL.js
+++ accionLFL.js
@@ -75,7 +75,7 @@ cargarGeoJSONCorregido('cauceELLIMON.geojson', estiloCauce, 'Cauce del río El L
 cargarGeoJSONCorregido('cuencaJOBA.geojson', estiloCuenca, 'Cuenca del río Joba');
 cargarGeoJSONCorregido('cauceJOBA.geojson', estiloCauce, 'Cauce del río Joba');
 cargarGeoJSONCorregido('cuencaMAIMONCIBAO.geojson', estiloCuenca, 'Cuenca del río Maimón Cibao');
-cargarGeoJSONCorregido('cauceMAINOMCIBAO.geojson', estiloCauce, 'Cauce del río Maimón Cibao');
+cargarGeoJSONCorregido('cauceMAIMONCIBAO.geojson', estiloCauce, 'Cauce del río Maimón Cibao');
 cargarGeoJSONCorregido('cuencaNAGUA.geojson', estiloCuenca, 'Cuenca del río Nagua');
 cargarGeoJSONCorregido('cauceNAGUA.geojson', estiloCauce, 'Cauce del río Nagua');
 cargarGeoJSONCorregido('cuencaSJUANCIBAO.geojson', estiloCuenca, 'Cuenca del río San Juan Cibao');
